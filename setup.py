4cad1cc161eed6b4cde08df70d788529a3457be0
Anubhav Jain <ajain@lbl.gov>
2013-01-09 02:05:05 -0800

    minimal structure for FireWorks package

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+Created on Jan 9, 2013
+'''
+
+__author__ = "Anubhav Jain"
+__copyright__ = "Copyright 2013, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Anubhav Jain"
+__email__ = "ajain@lbl.gov"
+__date__ = "Jan 9, 2013"
+
+from setuptools import setup
+
+setup(name='fireworks',
+      version='0.1',
+      description='FireWorks workflow software',
+      url='https://github.com/materialsproject/fireworks',
+      author='Anubhav Jain',
+      author_email='anubhavster@gmail.com',
+      license='MIT',
+      packages=['fireworks'],
+      zip_safe=False)
