37c1408f762fa55b67688628e8c8a1ac58dd4530
Anubhav Jain <ajain@lbl.gov>
2013-01-26 18:50:39 -0800

    add more serialization tests

--- fireworks/utilities/tests/test_fw_serializers.py
+++ fireworks/utilities/tests/test_fw_serializers.py
@@ -3,7 +3,7 @@
 '''
 TODO: add docs
 '''
-from fireworks.utilities.fw_serializers import FWSerializable, serialize_fw
+from fireworks.utilities.tests.test_serializer import TestSerializer
 
 
 __author__ = "Anubhav Jain"
@@ -16,44 +16,30 @@ __date__ = "Jan 26, 2013"
 import unittest
 import datetime
 
-# TODO: wrong module name....
-# TODO: test class name, fw_name, module name
+# TODO: unicode TEST
+# TODO: add file import/export test
+# TODO: add implicit and one-line serialization tests
+
 
 class SerializationTest(unittest.TestCase):
 
     def setUp(self):
-        
-        class TestSerializer(FWSerializable):
-            _fw_name = 'TestSerializer'
-            
-            def __init__(self, a, m_date):
-                if not isinstance(m_date, datetime.datetime):
-                    raise ValueError("m_date must be a datetime instance!")
-                
-                self.a = a
-                self.m_date = m_date
-            
-            def __eq__(self, other):
-                return self.a == other.a and self.m_date == other.m_date
-            
-            @serialize_fw
-            def to_dict(self):
-                return {"a": self.a, "m_date": self.m_date}
-            
-            @classmethod
-            def from_dict(self, m_dict):
-                return TestSerializer(m_dict["a"], m_dict["m_date"])
-
         test_date = datetime.datetime.utcnow()
         self.obj_1 = TestSerializer("prop1", test_date)
         self.obj_1_copy = TestSerializer("prop1", test_date)
-        self.obj_2 = TestSerializer("prop2", test_date)
+        self.obj_2 = TestSerializer({"p1": 1234, "p2": 5.0, "p3": "Hi!", 'p4': datetime.datetime.utcnow()}, test_date)
         
     def test_sanity(self):
         self.assertEqual(self.obj_1, self.obj_1_copy, "The __eq__() method of the TestSerializer is not set up properly!")
         self.assertNotEqual(self.obj_1, self.obj_2, "The __ne__() method of the TestSerializer is not set up properly!")
         self.assertEqual(self.obj_1, self.obj_1.from_dict(self.obj_1.to_dict()), "The to/from_dict() methods of the TestSerializer are not set up properly!")
     
+    def test_serialize_fw_decorator(self):
+        m_dict = self.obj_1.to_dict()
+        self.assertEqual(m_dict['_fw_name'], 'TestSerializer Name')
+        self.assertEqual(m_dict['@class'], 'TestSerializer')
+        self.assertEqual(m_dict['@module'], 'fireworks.utilities.tests.test_serializer')
+        
     def test_json(self):
         obj1_json_string = str(self.obj_1.to_format())  # default format is JSON, make sure this is true
         self.assertEqual(self.obj_1.from_format(obj1_json_string), self.obj_1, 'JSON format export / import fails!')
@@ -61,6 +47,14 @@ class SerializationTest(unittest.TestCase):
     def test_yaml(self):
         obj1_yaml_string = str(self.obj_1.to_format('yaml'))
         self.assertEqual(self.obj_1.from_format(obj1_yaml_string, 'yaml'), self.obj_1, 'YAML format export / import fails!')
-        
+    
+    def test_complex_json(self):
+        obj2_json_string = str(self.obj_2.to_format())  # default format is JSON, make sure this is true
+        self.assertEqual(self.obj_2.from_format(obj2_json_string), self.obj_2, 'Complex JSON format export / import fails!')
+    
+    def test_complex_yaml(self):
+        obj2_yaml_string = str(self.obj_2.to_format('yaml'))
+        self.assertEqual(self.obj_2.from_format(obj2_yaml_string, 'yaml'), self.obj_2, 'Complex YAML format export / import fails!')
+                
 if __name__ == "__main__":
     unittest.main()
