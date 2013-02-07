38e3ba68ebc17adceafa6f23fb7ecc9c942f9680
Anubhav Jain <ajain@lbl.gov>
2013-02-07 12:57:53 -0800

    add fw_id to fw, and a test fw to the tutorial dir

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -18,15 +18,22 @@ class FireWork(FWSerializable):
     
     _fw_name = "FireWork"
     
-    def __init__(self, fw_spec):
+    def __init__(self, fw_spec, fw_id=None):
         '''
         
         :param fw_spec:
         '''
         self.fw_spec = fw_spec
+        self.fw_id = fw_id
     
     def to_dict(self):
-        return {'fw_spec': self.fw_spec}
+        return {'fw_spec': self.fw_spec, 'fw_id': self.fw_id}
     
     def from_dict(self, m_dict):
-        return FireWork(m_dict['fw_spec'])
+        return FireWork(m_dict['fw_spec'], m_dict['fw_id'])
+
+
+if __name__ == '__main__':
+    fw_spec = {'_script': 'echo "howdy, your job launched successfully!" >> howdy.txt'}
+    fw = FireWork(fw_spec)
+    fw.to_file("fw_test.yaml")
