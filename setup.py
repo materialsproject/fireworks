06b06612c1f60adb67ec7eacb8e53699a088fddf
Anubhav Jain <ajain@lbl.gov>
2013-02-07 21:37:01 -0800

    add if __name__ == main to avoid weird sphinxdoc / autodoc conflict

--- setup.py
+++ setup.py
@@ -12,29 +12,29 @@ import os
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
-
-setup(name='FireWorks',
-      version='0.1dev0.8',
-      description='FireWorks workflow software',
-      long_description=open(os.path.join(module_dir, 'README.rst')).read(),
-      url='https://github.com/materialsproject/fireworks',
-      author='Anubhav Jain',
-      author_email='anubhavster@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=['pyyaml', 'simplejson', 'pymongo'],
-      classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Other/Nonlisted Topic",
-        "Topic :: Scientific/Engineering"],
-        test_suite='nose.collector',
-        tests_require=['nose'],
-        scripts=[os.path.join(os.path.join(module_dir, "scripts", f)) for f in os.listdir(os.path.join(module_dir, "scripts"))]
-        )
+if __name__ == "__main__":
+    setup(name='FireWorks',
+          version='0.1dev0.8',
+          description='FireWorks workflow software',
+          long_description=open(os.path.join(module_dir, 'README.rst')).read(),
+          url='https://github.com/materialsproject/fireworks',
+          author='Anubhav Jain',
+          author_email='anubhavster@gmail.com',
+          license='MIT',
+          packages=find_packages(),
+          zip_safe=False,
+          install_requires=['pyyaml', 'simplejson', 'pymongo'],
+          classifiers=[
+            "Programming Language :: Python :: 2.7",
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: System Administrators",
+            "Intended Audience :: Information Technology",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Topic :: Other/Nonlisted Topic",
+            "Topic :: Scientific/Engineering"],
+            test_suite='nose.collector',
+            tests_require=['nose'],
+            scripts=[os.path.join(os.path.join(module_dir, "scripts", f)) for f in os.listdir(os.path.join(module_dir, "scripts"))]
+            )
