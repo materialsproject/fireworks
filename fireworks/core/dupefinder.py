8bbd3aeafeceb6fcd144abb06e32cd737c4aa592
Anubhav Jain <ajain@lbl.gov>
2013-03-01 16:42:07 -0800

    sketch out dupefinder framework

---
+++ fireworks/core/dupefinder.py
@@ -0,0 +1,31 @@
+from fireworks.utilities.fw_serializers import serialize_fw
+
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'Mar 01, 2013'
+
+
+class DupeFinderBase():
+    """
+    TODO: add docs
+    """
+
+    def __init__(self):
+        pass
+
+    def verify(self, spec1, spec2):
+        raise NotImplementedError
+
+    def query(self):
+        raise NotImplementedError
+
+    @serialize_fw
+    def to_dict(self):
+        return {}
+
+    @classmethod
+    def from_dict(cls, m_dict):
+        return cls()
---
+++ fireworks/user_objects/dupefinders/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'Mar 01, 2013'
