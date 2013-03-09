f11385bf48dfcc5a4a5f02ecd7837548c474e9f6
Anubhav Jain <ajain@lbl.gov>
2013-03-09 07:15:15 -0800

    raise Error if you try to run rapidfire in reservation mode

--- fireworks/queue/queue_launcher.py
+++ fireworks/queue/queue_launcher.py
@@ -65,6 +65,8 @@ def launch_rocket_to_queue(queue_params, launcher_dir='.', strm_lvl=None, launch
                     # TODO: make sure this does not affect future FireWorks!!
                     queue_params.params.update(fw.spec['_queueparams'])
                 # update the exe to include the FW_id
+                if 'singleshot' not in queue_params.params['exe']:
+                    raise ValueError('Reservation mode of queue launcher only works for singleshot Rocket Launcher!')
                 queue_params.params['exe'] += ' --fw_id {}'.format(fw.fw_id)
 
             # write and submit the queue script using the queue adapter
