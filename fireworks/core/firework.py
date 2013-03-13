709fd96c072f80b4d7da44441ad8ddefadb26ee5
Anubhav Jain <ajain@lbl.gov>
2013-03-12 22:34:07 -0700

    refactor LAUNCH_RANKS location

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -155,12 +155,12 @@ class FireWork(FWSerializable):
 
         return FireWork(tasks, m_dict['spec'], fw_id, l, state, created_at)
 
-# 'Canceled' is the dominant spelling over 'cancelled' in the US starting around 1985...(Google n-grams)
-LAUNCH_RANKS = {'DEFUSED': 0, 'WAITING': 1, 'READY': 2, 'FIZZLED': 3, 'RESERVED': 4, 'RUNNING': 5, 'CANCELED': 6,
-                'COMPLETED': 7}
-
 
 class Launch(FWSerializable):
+    # 'Canceled' is the dominant spelling over 'cancelled' in the US starting around 1985...(Google n-grams)
+    LAUNCH_RANKS = {'DEFUSED': 0, 'WAITING': 1, 'READY': 2, 'FIZZLED': 3, 'RESERVED': 4, 'RUNNING': 5, 'CANCELED': 6,
+                    'COMPLETED': 7}
+
     # TODO: add an expiration date
     # TODO: figure out a way to track how long it took to go from a RESERVED launch to a RUNNING launch. Also RUNTIME
     # TODO: Probably want a STATE_HISTORY.
@@ -177,7 +177,7 @@ class Launch(FWSerializable):
         :param state: the state of the Launch
         :param launch_id: the id of the Launch for the LaunchPad
         """
-        if state not in LAUNCH_RANKS:
+        if state not in Launch.LAUNCH_RANKS:
             raise ValueError("Invalid launch state: {}".format(state))
 
         self.fworker = fworker
