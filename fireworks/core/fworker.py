4c4d4fd8616738117e6755e8ca5ccdac66dd56ec
Anubhav Jain <ajain@lbl.gov>
2013-04-03 15:37:46 -0700

    remove params from FWorker

--- fireworks/core/fworker.py
+++ fireworks/core/fworker.py
@@ -18,24 +18,21 @@ __date__ = 'Dec 12, 2012'
 
 
 class FWorker(FWSerializable):
-    def __init__(self, name="anonymous worker", category="general worker", query=None, params=None):
+    def __init__(self, name="anonymous worker", category="general worker", query=None):
         """
         :param name: the name of the resource, should be unique
         :param category: a String describing the computing resource, does not need to be unique
         :param query: a dict query that restricts the type of FireWork this resource will run
-        :param params: further descriptions of this resource
         """
         self.name = name
         self.category = category
         self.query = query if query else {}
-        self.params = params
 
     @recursive_serialize
     def to_dict(self):
-        return {'name': self.name, 'category': self.category, 'query': json.dumps(self.query, default=DATETIME_HANDLER),
-                'params': self.params}
+        return {'name': self.name, 'category': self.category, 'query': json.dumps(self.query, default=DATETIME_HANDLER)}
 
     @classmethod
     @recursive_deserialize
     def from_dict(cls, m_dict):
-        return FWorker(m_dict['name'], m_dict['category'], json.loads(m_dict['query']), m_dict['params'])
+        return FWorker(m_dict['name'], m_dict['category'], json.loads(m_dict['query']))
