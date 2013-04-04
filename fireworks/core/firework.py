6c36acd7b414388b35ad5e98f732db2f11a2f277
Anubhav Jain <ajain@lbl.gov>
2013-04-04 16:06:38 -0700

    cleanup potential FWActions

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -64,7 +64,7 @@ class FWAction(FWSerializable):
      FWAction allows a user to store rudimentary output data as well as return commands that alter the workflow.
     """
 
-    commands = ['CONTINUE', 'DEFUSE', 'MODIFY', 'DETOUR', 'CREATE', 'PHOENIX', 'BREAK']
+    commands = ['CONTINUE', 'DEFUSE', 'MODIFY', 'BREAK']
 
     def __init__(self, command, stored_data=None, mod_spec=None):
         """
