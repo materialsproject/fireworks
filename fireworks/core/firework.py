97f3ee694ecc7bc5a68fb8c4365cf49dcc6def9f
Anubhav Jain <ajain@lbl.gov>
2013-05-03 16:10:10 -0700

    minor change - double dash to display string

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -766,7 +766,7 @@ class Workflow(FWSerializable):
 
     def to_display_dict(self):
         m_dict = self.to_db_dict()
-        m_dict['states'] = [{self.id_fw[x].name+'-'+str(x):self.id_fw[x].state} for x in m_dict['nodes']]
+        m_dict['states'] = [{self.id_fw[x].name+'--'+str(x):self.id_fw[x].state} for x in m_dict['nodes']]
         m_dict['nodes'] = [self.id_fw[x].name for x in m_dict['nodes']]
         m_dict['links'] = dict([(self.id_fw[int(k)].name, [self.id_fw[int(v)].name for v in a]) for k, a in m_dict['links'].iteritems()])
         m_dict['parent_links'] = dict([(self.id_fw[int(k)].name, [self.id_fw[int(v)].name for v in a]) for k, a in m_dict['parent_links'].iteritems()])
