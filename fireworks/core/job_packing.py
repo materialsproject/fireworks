57d870639effb2a57019a5bc42af5b391308c12b
Xiaohui Qu <xqu@lbl.gov>
2013-08-21 15:45:42 -0700

    add documentation

--- fireworks/core/job_packing.py
+++ fireworks/core/job_packing.py
@@ -1,6 +1,8 @@
 """
-Support modules for job packing.
-PackingManager provides share objects for all the processing.
+Support module for job packing.
+This module contains function to prepare and launch the process.
+Also, there is a PackingManager class which provides share objects
+Between processes.
 """
 from multiprocessing import Process
 from multiprocessing.managers import BaseManager
@@ -19,12 +21,37 @@ __date__ = 'Aug 19, 2013'
 
 
 class PackingManager(BaseManager):
+    '''
+    Customized Manager class.
+    It spawns a child process which can be used as a server process to
+    provide shared objects.
+    When registered with the LaunchPad typeid, it will be able to return
+    a proxy for the LaunchPad running in the server process. All the access
+    for other processes will be forward to the instance in the server process.
+    This is how the inter-process Singleton is implemented. Please be noted
+    that the decorator singleton pattern only works for single process programs.
+    Also, please noted this class has nothing to do with process management. Its
+    only role is to provide shared objects.
+    Example:
+        m = PackingManager(address=('127.0.0.1', port), authkey=password)
+        m.connect()
+        launchpad = m.LaunchPad()
+    '''
     pass
 
 
 PackingManager.register('LaunchPad')
 
 def create_launchpad(launchpad_file, strm_lvl):
+    '''
+    Function to create the server side LaunchPad instance.
+    This function will be called only once, only by the
+    Manager server process.
+
+    :param launchpad_file: (str) path to launchpad file
+    :param strm_lvl: (str) level at which to output logs to stdout
+    :return: (LaunchPad) object
+    '''
     if launchpad_file:
         launchpad = LaunchPad.from_file(launchpad_file)
     else:
@@ -33,11 +60,26 @@ def create_launchpad(launchpad_file, strm_lvl):
 
 
 def manager_initializer():
+    '''
+    The intialization function for Manager server process.
+    :return:
+    '''
     fw_conf = FWConfig()
     fw_conf.MULTIPROCESSING = None # don't confuse the server process
 
 
 def run_manager_server(lauchpad_file, strm_lvl, port, password):
+    '''
+    Start the Manager server process. The shared LaunchPad object proxy will
+    be available after calling this function. Nothing to do with process
+    management.
+
+    :param lauchpad_file: (str) path to launchpad file
+    :param strm_lvl: (str) level at which to output logs
+    :param port: (int) Listening port number
+    :param password: (str) security password to access the server
+    :return: (PackingManager) object
+    '''
     PackingManager.register('LaunchPad', callable=lambda: create_launchpad(lauchpad_file, strm_lvl))
     m = PackingManager(address=('127.0.0.1', port), authkey=password)
     m.start(initializer=manager_initializer)
@@ -45,6 +87,19 @@ def run_manager_server(lauchpad_file, strm_lvl, port, password):
 
 
 def rapidfire_process(fworker, nlaunches, sleep, loglvl, port, password, node_list, lock):
+    '''
+    Starting point of a sub job launching process.
+
+    :param fworker: (FWorker) object
+    :param nlaunches: (int) 0 means 'until completion', -1 or "infinite" means to loop forever
+    :param sleep: (int) secs to sleep between rapidfire loop iterations
+    :param loglvl: (str) level at which to output logs to stdout
+    :param port: (int) Listening port number
+    :param password: (str) security password to access the server
+    :param node_list: (list of str) computer node list
+    :param lock: (multiprocessing.Lock) Mutex
+    :return:
+    '''
     fw_conf = FWConfig()
     fw_conf.MULTIPROCESSING = True
     fw_conf.PACKING_MANAGER_PORT = port
@@ -58,6 +113,18 @@ def rapidfire_process(fworker, nlaunches, sleep, loglvl, port, password, node_li
 
 
 def launch_rapidfire_processes(fworker, nlaunches, sleep, loglvl, port, password, node_lists):
+    '''
+    Create the sub job launching processes
+
+    :param fworker: (FWorker) object
+    :param nlaunches: nlaunches: (int) 0 means 'until completion', -1 or "infinite" means to loop forever
+    :param sleep: (int) secs to sleep between rapidfire loop iterations
+    :param loglvl: (str) level at which to output logs to stdout
+    :param port: (int) Listening port number
+    :param password: (str) security password to access the server
+    :param node_lists: (list of str) computer node list
+    :return: (List of multiprocessing.Process) all the created processes
+    '''
     lock = multiprocessing.Lock()
     processes = [Process(target=rapidfire_process, args=(fworker, nlaunches, sleep, loglvl, port, password, nl, lock))
                  for nl in node_lists]
@@ -67,10 +134,13 @@ def launch_rapidfire_processes(fworker, nlaunches, sleep, loglvl, port, password
 
 
 def split_node_lists(num_rockets):
-    node_lists = None
+    '''
+    Allocate node list of the large job to the sub jobs
+    :param num_rockets: (int) number of sub jobs
+    :return: (list of list) NODELISTs
+    '''
     if 'PBS_NODEFILE' in os.environ:
         node_file = os.environ['PBS_NODEFILE']
-        orig_node_list = None
         with open(node_file) as f:
             orig_node_list = [line.strip() for line in f.readlines()]
         n = len(orig_node_list)
--- fireworks/scripts/mlaunch_run.py
+++ fireworks/scripts/mlaunch_run.py
@@ -16,7 +16,20 @@ __email__ = 'xqu@lbl.gov'
 __date__ = 'Aug 19, 2013'
 
 
-def launch_job_packing_processes(fworker, launchpad_file, loglvl, nlaunches, num_rockets, password, port, sleep_time):
+def launch_job_packing_processes(fworker, launchpad_file, loglvl, nlaunches,
+                                 num_rockets, password, port, sleep_time):
+    '''
+    Launch the jobs in the job packing mode.
+    :param fworker: (FWorker) object
+    :param launchpad_file: (str) path to launchpad file
+    :param loglvl: (str) level at which to output logs
+    :param nlaunches: (int) 0 means 'until completion', -1 or "infinite" means to loop forever
+    :param num_rockets: (int) number of sub jobs
+    :param password: (str) security password to access the shared object server
+    :param port: (str) security password to access the shared object server
+    :param sleep_time: (int) secs to sleep between rapidfire loop iterations
+    :return:
+    '''
     node_lists = split_node_lists(num_rockets)
     m = run_manager_server(launchpad_file, loglvl, port, password)
     processes = launch_rapidfire_processes(fworker, nlaunches, sleep_time, loglvl,
@@ -62,15 +75,8 @@ def mlaunch():
     else:
         fworker = FWorker()
 
-    num_rockets = args.num_rockets
-    launchpad_file = args.launchpad_file
-    loglvl = args.loglvl
-    port = args.port
-    password = args.password
-    nlaunches = args.nlaunches
-    sleep_time = args.sleep
-
-    launch_job_packing_processes(fworker, launchpad_file, loglvl, nlaunches, num_rockets, password, port, sleep_time)
+    launch_job_packing_processes(fworker, args.launchpad_file, args.loglvl, args.nlaunches,
+                                 args.num_rockets, args.password, args.port, args.sleep)
 
 
 if __name__ == "__main__":
