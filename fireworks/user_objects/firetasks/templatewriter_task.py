0e49287f0b0da0178bffa32a65bdad5ece1de13c
Anubhav Jain <ajain@lbl.gov>
2013-08-22 17:00:07 -0700

    add use_root to script_task and templatewriter_task

--- fireworks/user_objects/firetasks/templatewriter_task.py
+++ fireworks/user_objects/firetasks/templatewriter_task.py
@@ -30,23 +30,13 @@ class TemplateWriterTask(FireTaskBase, FWSerializable):
         """
         self.update(parameters)
 
-        self.context = self['context']
-        self.output_file = self['output_file']
-        self.append_file = self.get('append', False)  # append to output file?
-
-        if self.get('template_dir'):
-            self.template_dir = self['template_dir']
-        elif FWConfig().TEMPLATE_DIR:
-            self.template_dir = FWConfig().TEMPLATE_DIR
-        else:
-            MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
-            self.template_dir = os.path.join(MODULE_DIR, 'templates')
-
-        self.template_file = os.path.join(self.template_dir, self['template_file'])
-        if not os.path.exists(self.template_file):
-            raise ValueError("Template Writer Task could not find a template file at: {}".format(self.template_file))
+        if not parameters.get("use_root"):
+            self._load_parameters(parameters)
 
     def run_task(self, fw_spec):
+        if self.get("use_root"):
+            self._load_parameters(fw_spec)
+
         with open(self.template_file) as f:
             t = Template(f.read())
             output = t.render(Context(self.context))
@@ -54,3 +44,21 @@ class TemplateWriterTask(FireTaskBase, FWSerializable):
             write_mode = 'w+' if self.append_file else 'w'
             with open(self.output_file, write_mode) as of:
                 of.write(output)
+
+    def _load_parameters(self, params):
+
+        self.context = params['context']
+        self.output_file = params['output_file']
+        self.append_file = params.get('append')  # append to output file?
+
+        if params.get('template_dir'):
+            self.template_dir = params['template_dir']
+        elif FWConfig().TEMPLATE_DIR:
+            self.template_dir = FWConfig().TEMPLATE_DIR
+        else:
+            MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+            self.template_dir = os.path.join(MODULE_DIR, 'templates')
+
+        self.template_file = os.path.join(self.template_dir, params['template_file'])
+        if not os.path.exists(self.template_file):
+            raise ValueError("Template Writer Task could not find a template file at: {}".format(self.template_file))
