3979f3374d5dd5914e5bb5a7903bbecafd4919de
Anubhav Jain <ajain@lbl.gov>
2013-08-29 16:15:30 -0700

    start sketching out remote file transfer task

--- fireworks/user_objects/firetasks/transfer_task.py
+++ fireworks/user_objects/firetasks/transfer_task.py
@@ -1,21 +1,16 @@
+"""
+This module contains the TransferTask used to move and copy files. Remote transfer is also supported.
+"""
+
+import os
 from os.path import expandvars, expanduser
 import shutil
-
-__author__ = 'Anubhav Jain'
-__copyright__ = 'Copyright 2013, The Materials Project'
-__version__ = '0.1'
-__maintainer__ = 'Anubhav Jain'
-__email__ = 'ajain@lbl.gov'
-__date__ = 'Aug 29, 2013'
-
-"""
-This module contains the MoverTask, which moves files and directories and supports remote file movement through SSH2 via paramiko
-"""
-
+import traceback
+import paramiko
 from fireworks.core.firework import FireTaskBase
 from fireworks.utilities.fw_serializers import FWSerializable
 
-__author__ = 'Anubhav Jain'
+__author__ = 'Anubhav Jain, David Waroquiers'
 __copyright__ = 'Copyright 2013, The Materials Project'
 __version__ = '0.1'
 __maintainer__ = 'Anubhav Jain'
@@ -26,21 +21,22 @@ __date__ = 'Aug 29, 2013'
 class TransferTask(FireTaskBase, FWSerializable):
     _fw_name = "Transfer Task"
 
-    fn_list = {
-        "move": shutil.move,
-        "mv": shutil.move,
-        "copy": shutil.copy,
-        "cp": shutil.copy,
-        "copy2": shutil.copy2,
-        "copytree": shutil.copytree,
-        "copyfile": shutil.copyfile,
-    }
-
     def __init__(self, parameters):
         """
         :param parameters: (dict) parameters.
         """
         self.update(parameters)
+
+        self.fn_list = {
+            "move": shutil.move,
+            "mv": shutil.move,
+            "copy": shutil.copy,
+            "cp": shutil.copy,
+            "copy2": shutil.copy2,
+            "copytree": shutil.copytree,
+            "copyfile": shutil.copyfile,
+            "rtransfer": self._remotetransfer
+        }
 
         if not parameters.get("use_root"):
             self._load_parameters(parameters)
@@ -49,22 +45,68 @@ class TransferTask(FireTaskBase, FWSerializable):
         if self.get("use_root"):
             self._load_parameters(fw_spec)
 
-        for f in self.files:
-            o = self.options
-            shell_interpret = o.get('shell_interpret', True)
-            src = expanduser(expandvars(f['src'])) if shell_interpret else expandvars(f['src'])
-            dest = expanduser(expandvars(f['dest'])) if shell_interpret else expandvars(f['dest'])
-            ignore_errors = o.get('ignore_errors')
-            mode = o.get('mode', 'move')
+        o = self.options
 
-            try:
-                self.fn_list[mode](src, dest)
-            except:
-                if not ignore_errors:
-                    raise ValueError(
-                        "There was an error performing operation {} from {} to {}".format(mode, src,
-                                                                                          dest))
+        if o.get('mode') == 'rtransfer':
+            # remote transfers
+            pass
+
+        else:
+            # local transfers
+            for f in self.files:
+                shell_interpret = o.get('shell_interpret', True)
+                src = expanduser(expandvars(f['src'])) if shell_interpret else expandvars(f['src'])
+                dest = expanduser(expandvars(f['dest'])) if shell_interpret else expandvars(f['dest'])
+                ignore_errors = o.get('ignore_errors')
+                mode = o.get('mode', 'move')
+
+                try:
+                    self.fn_list[mode](src, dest)
+                except:
+                    if not ignore_errors:
+                        raise ValueError(
+                            "There was an error performing operation {} from {} to {}".format(mode, src,
+                                                                                              dest))
 
     def _load_parameters(self, params):
         self.options = params.get('options', {})
         self.files = params['files']
+
+    def _rexists(sftp, path):
+        """
+        os.path.exists for paramiko's SCP object
+        """
+        try:
+            sftp.stat(path)
+        except IOError, e:
+            if e[0] == 2:
+                return False
+            raise
+        else:
+            return True
+
+    def _remotetransfer(self, src, dest, o):
+        # Connecting to the server
+        ssh = paramiko.SSHClient()
+        ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        ssh.connect(o['server'], username=o['username'], port=o['port'], key_filename=o['key_filename'])
+
+        # Create SFTP connection
+        sftp = ssh.open_sftp()
+
+        # Local and remote directories
+        try:
+            # Transfer files from local directory to remote directory
+            if not self._rexists(sftp, dest):
+                sftp.mkdir(dest)
+
+            sftp.put(src, dest)
+
+        except:
+            traceback.print_exc()
+            raise ValueError('Error during remote transfer from {} to {} on server {} '.format(src, dest, o['server']))
+
+        finally:
+            # Close the connections
+            sftp.close()
+            ssh.close()
