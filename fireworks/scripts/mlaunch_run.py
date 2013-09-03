693c66d487e5fbe396a914b68325aefff2ddfb49
Xiaohui Qu <xqu@lbl.gov>
2013-09-03 15:41:17 -0700

    fix the serial mode command line parse bug

--- fireworks/scripts/mlaunch_run.py
+++ fireworks/scripts/mlaunch_run.py
@@ -41,7 +41,7 @@ def mlaunch():
     parser.add_argument('--num_rockets', help='the numbers of sub jobs to split into', default=2, type=int)
     parser.add_argument('--nodefile_env', help='the environemntal variable name containing the node file name', default=None, type=str)
     parser.add_argument('--ppn', help='processors per node', default=24, type=int)
-    parser.add_argument('--serial_mode', help='is the sub job a serials one?', default=False, type=bool)
+    parser.add_argument('--serial_mode', help='is the sub job a serials one?', action='store_true', default=False)
 
     args = parser.parse_args()
 
