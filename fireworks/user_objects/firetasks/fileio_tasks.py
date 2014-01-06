36cb1dca1ade47ffdaeceecb9d3af64cc754de1d
__sp__ <shyuep@gmail.com>
2014-01-06 11:52:32 -0800

    Refactor and add FileDelete Task.

--- fireworks/user_objects/firetasks/fileio_tasks.py
+++ fireworks/user_objects/firetasks/fileio_tasks.py
@@ -10,8 +10,8 @@ __email__ = 'ongsp@ucsd.edu'
 __date__ = 'Jan 6, 2014'
 
 
-class FileWriterTask(FireTaskBase, FWSerializable):
-    _fw_name = "File Writer Task"
+class FileWriteTask(FireTaskBase, FWSerializable):
+    _fw_name = "File Write Task"
 
     def __init__(self, parameters=None):
         """
@@ -24,6 +24,24 @@ class FileWriterTask(FireTaskBase, FWSerializable):
 
     def run_task(self, fw_spec):
         pth = self.get("dest", os.getcwd())
-        for d in fw_spec["input_files"]:
+        for d in fw_spec["files_to_write"]:
             with open(os.path.join(pth, d["filename"]), "w") as f:
                 f.write(d["contents"])
+
+
+class FileDeleteTask(FireTaskBase, FWSerializable):
+    _fw_name = "File Delete Task"
+
+    def __init__(self, parameters=None):
+        """
+        :param parameters: Optional dict parameters. The only supported one
+        is "dest", which should point to the location where the files are to
+        be deleted. If left blank, the current working directory is used.
+        """
+        if parameters is not None:
+            self.update(parameters)
+
+    def run_task(self, fw_spec):
+        pth = self.get("dest", os.getcwd())
+        for f in fw_spec["files_to_delete"]:
+            os.remove(os.path.join(pth, f))
--- fireworks/user_objects/firetasks/tests/test_fileio_tasks.py
+++ fireworks/user_objects/firetasks/tests/test_fileio_tasks.py
@@ -8,21 +8,27 @@ __date__ = '1/6/14'
 import unittest
 import os
 
-from fireworks.user_objects.firetasks.fileio_tasks import FileWriterTask
+from fireworks.user_objects.firetasks.fileio_tasks import FileWriteTask, FileDeleteTask
 from fireworks.utilities.fw_serializers import load_object_from_file
 
 
 module_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-class FileWriterTest(unittest.TestCase):
+class FileWriteDeleteTest(unittest.TestCase):
 
     def test_run(self):
-        t = load_object_from_file(os.path.join(module_dir, "writer.yaml"))
+        t = load_object_from_file(os.path.join(module_dir, "write.yaml"))
         t.run_task(t)
         for i in xrange(2):
             self.assertTrue(os.path.exists("myfile{}".format(i + 1)))
-            os.remove("myfile{}".format(i + 1))
+
+        #Use delete task to remove the files created.
+        t = load_object_from_file(os.path.join(module_dir, "delete.yaml"))
+        t.run_task(t)
+        for i in xrange(2):
+            self.assertFalse(os.path.exists("myfile{}".format(i + 1)))
+
 
 if __name__ == '__main__':
     unittest.main()
