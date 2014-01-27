e39da9ea364e3f161f4e3600d24936453e4673bf
__sp__ <shyuep@gmail.com>
2014-01-26 23:10:44 -0800

    Add relative improts in root __init__.py

--- fireworks/__init__.py
+++ fireworks/__init__.py
@@ -2,3 +2,9 @@ import os
 
 __version__ = '0.66'
 FW_INSTALL_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# These imports allow a much simpler import of core Fireworks functionality.
+# E.g., you can now do "from fireworks import FireWork", instead of from
+# "fireworks.core.firework import FireWork".
+from fireworks.core.firework import FireTaskBase, FireWork, Launch, Workflow
+from fireworks.core.launchpad import LaunchPad
