9a2e8449d5f76088693e4f8629f3b0e866244859
__sp__ <shyuep@gmail.com>
2014-01-27 10:01:43 -0800

    Minor fix for GUI and add more root level imports.

--- fireworks/__init__.py
+++ fireworks/__init__.py
@@ -6,5 +6,7 @@ FW_INSTALL_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # These imports allow a much simpler import of core Fireworks functionality.
 # E.g., you can now do "from fireworks import FireWork", instead of from
 # "fireworks.core.firework import FireWork".
-from fireworks.core.firework import FireTaskBase, FireWork, Launch, Workflow
+from fireworks.core.firework import FireTaskBase, FireWork, Launch, Workflow,\
+    FWAction
 from fireworks.core.launchpad import LaunchPad
+from fireworks.utilities.fw_utilities import explicit_serialize
