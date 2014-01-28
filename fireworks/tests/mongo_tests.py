78a27f2a00f2918eee70484b921d114d238e5bdb
__sp__ <shyuep@gmail.com>
2014-01-27 23:31:43 -0800

    Still experimenting with unicode.

--- fireworks/tests/mongo_tests.py
+++ fireworks/tests/mongo_tests.py
@@ -49,7 +49,7 @@ class MongoTests(unittest.TestCase):
         self.lp.add_wf(fw)
         launch_rocket(self.lp, self.fworker)
         self.assertEqual(self.lp.get_launch_by_id(1).action.stored_data[
-            'stdout'].decode("utf-8"), str('test1\n'))
+            'stdout'], 'test1\n')
 
     def test_multi_fw(self):
         test1 = ScriptTask.from_str("python -c 'print(\"test1\")'",
@@ -59,7 +59,7 @@ class MongoTests(unittest.TestCase):
         fw = FireWork([test1, test2])
         self.lp.add_wf(fw)
         launch_rocket(self.lp, self.fworker)
-        self.assertEqual(self.lp.get_launch_by_id(1).action.stored_data['stdout'].decode("utf-8"), str('test2\n'))
+        self.assertEqual(self.lp.get_launch_by_id(1).action.stored_data['stdout'], 'test2\n')
 
     def test_add_fw(self):
         fw = FireWork(AdditionTask(), {'input_array': [5, 7]})
@@ -77,7 +77,7 @@ class MongoTests(unittest.TestCase):
         wf = Workflow([fw1, fw2], {-1: -2})
         self.lp.add_wf(wf)
         launch_rocket(self.lp, self.fworker)
-        self.assertEqual(self.lp.get_launch_by_id(1).action.stored_data['stdout'].decode("utf-8"), str('test1\n'))
+        self.assertEqual(self.lp.get_launch_by_id(1).action.stored_data['stdout'], 'test1\n')
         launch_rocket(self.lp, self.fworker)
         self.assertEqual(self.lp.get_launch_by_id(2).action.stored_data['stdout'].decode("utf-8"), str('test2\n'))
 
