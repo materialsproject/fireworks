a8466381a295348df561e2ed70cd11e47949ff33
__sp__ <shyuep@gmail.com>
2014-02-11 09:36:31 -0800

    Improved cmd line test.

--- fireworks/tests/cmd_line_test.sh
+++ fireworks/tests/cmd_line_test.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-lpad add fw_tutorials/firetask/fw_adder.yaml
+function test_cmd {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1"
+        exit -1
+    fi
+    return $status
+}
+
+test_cmd lpad reset --password `date "+%Y-%m-%d"`
+test_cmd lpad add fw_tutorials/firetask/fw_adder.yaml
 for mode in "more" "less" "all"
 do
-    lpad get_wfs -d $mode
-    lpad get_fws -d $mode
+    test_cmd lpad get_wfs -d $mode
+    test_cmd lpad get_fws -d $mode
 done
 rlaunch singleshot
