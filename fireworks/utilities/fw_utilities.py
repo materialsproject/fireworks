f013b8558f8a0de64f2258707c98a257dd54a4d3
Anubhav Jain <ajain@lbl.gov>
2014-02-10 17:33:42 -0800

    slight mod to prev workaround

--- fireworks/utilities/fw_utilities.py
+++ fireworks/utilities/fw_utilities.py
@@ -134,10 +134,11 @@ def create_datestamp_dir(root_dir, l_logger, prefix='block_'):
     try:
         os.mkdir(full_path)
     except OSError, e:
-        if e.errno != 17:
+        if e.errno != 17:  # File exists error
             raise
         import time
-        time.sleep(0.2)
+        import random
+        time.sleep(random.randrange(1, 5)*0.1)
         full_path = get_path()
         os.mkdir(full_path)
 
