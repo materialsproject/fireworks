478494268eea646fd2c282d076d2f5550c582001
Anubhav Jain <ajain@lbl.gov>
2014-02-12 10:06:54 -0800

    include templates in pip install

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         author_email='anubhavster@gmail.com',
         license='modified BSD',
         packages=find_packages(),
-        package_data={'fireworks':['user_objects/queue_adapters/*.txt']},
+        package_data={'fireworks':['user_objects/queue_adapters/*.txt', 'user_objects/firetasks/templates/*']},
         zip_safe=False,
         install_requires=['pyyaml>=3.1.0', 'pymongo>=2.4.2', 'Jinja2>=2.7.1',
                           'six>=1.5.2', 'monty>=0.1.1'],
