e9f2f8ade4bff2ac95a9875237d794078f8f901d
Bharat K. Medasani <mbkumar@gmail.com>
2014-08-14 13:38:36 -0700

    Changed folders for test_tracker.py

--- fireworks/scripts/tests/test_tracker.py
+++ fireworks/scripts/tests/test_tracker.py
@@ -50,6 +50,37 @@ class TrackerTest(unittest.TestCase):
 
     def setUp(self):
         self.old_wd = os.getcwd()
+        self.dest3 = os.path.join(MODULE_DIR,'test_launchpad.yaml')
+        self.lp.to_file(self.dest3)
+
+        # Setup the argument parser
+        fw_id_args = ["-i", "--fw_id"]
+        fw_id_kwargs = {"type": int, "nargs": "+", "help": "fw_id"}
+
+        state_args = ['-s', '--state']
+        state_kwargs = {"type": str.upper, "help": "Select by state.",
+                        "choices": FireWork.STATE_RANKS.keys()}
+        disp_args = ['-d', '--display_format']
+        disp_kwargs = {"type": str, "help": "Display format.", "default": "less",
+                       "choices": ["all", "more", "less", "ids", "count"]}
+
+        query_args = ["-q", "--query"]
+        query_kwargs = {"help": 'Query (enclose pymongo-style dict in '
+                                'single-quotes, e.g. \'{"state":"COMPLETED"}\')'}
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument(*fw_id_args, **fw_id_kwargs)
+        parser.add_argument('-n', '--name')
+        parser.add_argument(*state_args, **state_kwargs)
+        parser.add_argument(*query_args, **query_kwargs)
+        parser.add_argument('-c', '--include', nargs="+",
+                            help='only include these files in the report')
+        parser.add_argument('-x', '--exclude', nargs="+",
+                            help='exclude these files from the report')
+        parser.add_argument('--launchpad_file', default=self.dest3)
+
+        self.parser = parser
+
 
     def tearDown(self):
         self.lp.reset(password=None, require_password=False)
@@ -58,6 +89,8 @@ class TrackerTest(unittest.TestCase):
         os.chdir(self.old_wd)
         for i in glob.glob(os.path.join(MODULE_DIR,'launcher_*')):
             shutil.rmtree(i)
+        if os.path.exists(self.dest3):
+            os.remove(self.dest3)
 
     def _teardown(self, dests):
         for f in dests:
@@ -65,10 +98,12 @@ class TrackerTest(unittest.TestCase):
                 os.remove(f)
 
     def test_tracker(self):
+        """
+        Launch a workflow and track the files
+        """
 
         dest1 = os.path.join(MODULE_DIR, 'inputs.txt')
         dest2 = os.path.join(MODULE_DIR, 'words.txt')
-        dest3 = os.path.join(MODULE_DIR,'test_launchpad.yaml')
         dest4 = os.path.join(MODULE_DIR,'tmp_log.txt')
         self._teardown([dest1,dest2])
         fts =  []
@@ -84,39 +119,10 @@ class TrackerTest(unittest.TestCase):
 
             fw = FireWork(fts, spec={'_trackers':[tracker1,tracker2]}, fw_id=20, name='test_fw')
             self.lp.add_wf(fw)
-            self.lp.to_file(dest3)
             fw_ids = self.lp.get_fw_ids(query={'name':'test_fw'})
-            #print 'length', len(fw_ids)
-            fw_id = fw_ids[0]
-            #print fw_id
             launch_rocket(self.lp, self.fworker)
 
-            fw_id_args = ["-i", "--fw_id"]
-            fw_id_kwargs = {"type": int, "nargs": "+", "help": "fw_id"}
-
-            state_args = ['-s', '--state']
-            state_kwargs = {"type": str.upper, "help": "Select by state.",
-                            "choices": FireWork.STATE_RANKS.keys()}
-            disp_args = ['-d', '--display_format']
-            disp_kwargs = {"type": str, "help": "Display format.", "default": "less",
-                            "choices": ["all", "more", "less", "ids", "count"]}
-
-            query_args = ["-q", "--query"]
-            query_kwargs = {"help": 'Query (enclose pymongo-style dict in '
-                            'single-quotes, e.g. \'{"state":"COMPLETED"}\')'}
-
-            parser = argparse.ArgumentParser()
-            parser.add_argument(*fw_id_args, **fw_id_kwargs)
-            parser.add_argument('-n', '--name')
-            parser.add_argument(*state_args, **state_kwargs)
-            parser.add_argument(*query_args, **query_kwargs)
-            parser.add_argument('-c', '--include', nargs="+",
-                                        help='only include these files in the report')
-            parser.add_argument('-x', '--exclude', nargs="+",
-                                        help='exclude these files from the report')
-            parser.add_argument('--launchpad_file', default=dest3)
-
-            args = parser.parse_args('-i 1'.split())
+            args = self.parser.parse_args('-i 1'.split())
             with open(dest4,'w') as fp:
                 sys.stdout = fp
                 sys.stderr = fp
@@ -128,23 +134,20 @@ class TrackerTest(unittest.TestCase):
                          "98\n99\n## Launch id: 1\n"+\
                          "### Filename: "+dest1+"\noption1 = 99\n"+\
                          "option2 = fast method\n"
-            #print expected_output_string
             with open(dest4) as fp:
                 output = fp.read()
-                #print output
                 self.assertEqual(output,expected_output_string)
 
         finally:
-            self._teardown([dest1,dest2,dest3,dest4])
+            self._teardown([dest1,dest2,dest4])
 
     def test_tracker_failed_fw(self):
-
+        """
+        Add a bad firetask to workflow and test the tracking
+        """
         dest1 = os.path.join(MODULE_DIR, 'inputs.txt')
         dest2 = os.path.join(MODULE_DIR, 'words.txt')
-        dest3 = os.path.join(MODULE_DIR,'tmp_log.txt')
-        dest4 = os.path.join(MODULE_DIR,'test_launchpad.yaml')
-        #print 'MODULE_DIR', MODULE_DIR
-        #print dest1, dest2, dest3, dest4
+        dest4 = os.path.join(MODULE_DIR,'tmp_log.txt')
         self._teardown([dest1,dest2])
         fts =  []
         try:
@@ -167,42 +170,14 @@ class TrackerTest(unittest.TestCase):
 
             fw = FireWork(fts, spec={'_trackers':[tracker1,tracker2]}, fw_id=21, name='test_fw')
             self.lp.add_wf(fw)
-            self.lp.to_file(dest4)
-            fw_id = self.lp.get_fw_ids(query={'name':'test_fw'})[0]
-            #print fw_id
+
             try:
                 launch_rocket(self.lp, self.fworker)
             except:
-                #print 'error in running fw'
                 pass
 
-            fw_id_args = ["-i", "--fw_id"]
-            fw_id_kwargs = {"type": int, "nargs": "+", "help": "fw_id"}
-
-            state_args = ['-s', '--state']
-            state_kwargs = {"type": str.upper, "help": "Select by state.",
-                            "choices": FireWork.STATE_RANKS.keys()}
-            disp_args = ['-d', '--display_format']
-            disp_kwargs = {"type": str, "help": "Display format.", "default": "less",
-                            "choices": ["all", "more", "less", "ids", "count"]}
-
-            query_args = ["-q", "--query"]
-            query_kwargs = {"help": 'Query (enclose pymongo-style dict in '
-                            'single-quotes, e.g. \'{"state":"COMPLETED"}\')'}
-
-            parser = argparse.ArgumentParser()
-            parser.add_argument(*fw_id_args, **fw_id_kwargs)
-            parser.add_argument('-n', '--name')
-            parser.add_argument(*state_args, **state_kwargs)
-            parser.add_argument(*query_args, **query_kwargs)
-            parser.add_argument('-c', '--include', nargs="+",
-                                        help='only include these files in the report')
-            parser.add_argument('-x', '--exclude', nargs="+",
-                                        help='exclude these files from the report')
-            parser.add_argument('--launchpad_file', default=dest4)
-
-            args = parser.parse_args('-i 1'.split())
-            with open(dest3,'w') as fp:
+            args = self.parser.parse_args('-i 1'.split())
+            with open(dest4,'w') as fp:
                 sys.stdout = fp
                 sys.stderr = fp
                 try:
@@ -218,13 +193,12 @@ class TrackerTest(unittest.TestCase):
                          "### Filename: "+dest1+"\noption1 = 49\n"+\
                          "option2 = fast method\n"
             #print expected_output_string
-            with open(dest3) as fp:
+            with open(dest4) as fp:
                 output = fp.read()
-                #print output
                 self.assertEqual(output,expected_output_string)
 
         finally:
-            self._teardown([dest1,dest2,dest3,dest4])
+            self._teardown([dest1,dest2,dest4])
             pass
 
 
