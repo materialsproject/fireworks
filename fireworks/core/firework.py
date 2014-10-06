2e6f0abe1b4391667e6fa47845e0996a278aa727
__sp__ <ongsp@eng.ucsd.edu>
2014-10-06 06:46:31 -0700

    iUse reverse_readfile instead of reverse_readline.

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -23,7 +23,7 @@ from datetime import datetime
 import os
 import pprint
 
-from monty.io import reverse_readline, zopen
+from monty.io import reverse_readfile, zopen
 from monty.os.path import zpath
 from six import add_metaclass
 
@@ -347,11 +347,10 @@ class Tracker(FWSerializable, object):
 
         lines = []
         if os.path.exists(m_file):
-            with zopen(zpath(m_file)) as f:
-                for l in reverse_readline(f):
-                    lines.append(l)
-                    if len(lines) == self.nlines:
-                        break
+            for l in reverse_readfile(zpath(m_file)):
+                lines.append(l)
+                if len(lines) == self.nlines:
+                    break
             self.content = '\n'.join(reversed(lines))
 
         return self.content
