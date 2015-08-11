22284b5fbbc47f3e49840f67552c54ff25ba36b5
Patrick Huck <phuck@lbl.gov>
2015-08-11 16:19:41 -0700

    fworker: fix query when user sets or-query

--- fireworks/core/fworker.py
+++ fireworks/core/fworker.py
@@ -59,7 +59,12 @@ class FWorker(FWSerializable):
     @property
     def query(self):
         q = self._query
-        q['$or'] = [{"spec._fworker": {"$exists": False}}, {"spec._fworker": None}, {"spec._fworker": self.name}]
+        fworker_check = [{"spec._fworker": {"$exists": False}}, {"spec._fworker": None}, {"spec._fworker": self.name}]
+        if '$or' in q:
+            # TODO: cover case where user sets an $and query?
+            q['$and'] = [{'$or': q.pop('$or')}, {'$or': fworker_check}]
+        else:
+            q['$or'] = fworker_check
         if self.category:
             q['spec._category'] = self.category
         return q
