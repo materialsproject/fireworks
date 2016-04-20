5b6439dd92482b84df0066f80395d17c5e0d2ad7
Anubhav Jain <ajain@lbl.gov>
2016-04-19 21:31:40 -0700

    fix comment

--- fireworks/fw_config.py
+++ fireworks/fw_config.py
@@ -66,7 +66,7 @@ RAPIDFIRE_SLEEP_SECS = 60  # seconds to sleep between rapidfire loops
 
 LAUNCHPAD_LOC = None  # where to find the my_launchpad.yaml file
 FWORKER_LOC = None  # where to find the my_fworker.yaml file
-QUEUEADAPTER_LOC = None  # where to find the my_queueadapter.yaml file
+QUEUEADAPTER_LOC = None  # where to find the my_qadapter.yaml file
 
 CONFIG_FILE_DIR = '.'  # directory containing config files (if not individually set)
 
