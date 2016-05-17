44fb103a54674c638d27f198cc479d704d891c22
David Dotson <dotsdl@gmail.com>
2016-05-16 22:49:21 -0700

    Cleaned up Workflow copying machinery.
    Class method `Workflow.from_wflow` now much simpler, and doesn't rely on
    deepcopy. Also added `Workflow.reset` method for resetting states of all
    Fireworkers to default "WAITING" and optionally giving the new ids. This
    is used by `Workflow.from_wflow` to make the new Workflow unique from
    the one it was copied from.

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -1078,35 +1078,39 @@ class Workflow(FWSerializable):
         return m_launch
 
     @classmethod
-    def from_Workflow(cls, wflow):
-        """Create a fresh Workflow from a given one.
+    def from_wflow(cls, wflow):
+        """Create a fresh Workflow from an existing one.
 
         """
-        new_wf = Workflow([copy.deepcopy(fw) for fw in wflow.fws],
-                          links_dict=wflow.links,
-                          metadata=wflow.metadata,
-                          name=wflow.name)
 
-        # need to give Fireworks new ids
-        old_new = {} # mapping between old and new Firework ids
-
-        for fw_id, fw in new_wf.id_fw.items():
-            global NEGATIVE_FWID_CTR
-            NEGATIVE_FWID_CTR -= 1
-            new_id = NEGATIVE_FWID_CTR
-
-            old_new[fw_id] = new_id
-            fw.fw_id = new_id
-        
-        new_wf._reassign_ids(old_new)
-
-        # reset states
-        for fw in new_wf.fws:
-            fw.state = 'WAITING'
-
-        new_wf.fw_states = {key: new_wf.id_fw[key].state for key in new_wf.id_fw}
+        new_wf = Workflow.from_dict(wflow.to_dict())
+        new_wf.reset(reset_ids=True)
 
         return new_wf
+
+    def reset(self, reset_ids=False):
+        """Reset the states of all Fireworks in this workflow to 'WAITING'.
+
+        :param rest_ids: (bool) if ``True``, give each Firework a new id.
+
+        """
+        if rest_ids:
+            old_new = {} # mapping between old and new Firework ids
+            for fw_id, fw in self.id_fw.items():
+                global NEGATIVE_FWID_CTR
+                NEGATIVE_FWID_CTR -= 1
+                new_id = NEGATIVE_FWID_CTR
+
+                old_new[fw_id] = new_id
+                fw.fw_id = new_id
+        
+            self._reassign_ids(old_new)
+
+        # reset states
+        for fw in self.fws:
+            fw.state = 'WAITING'
+
+        self.fw_states = {key: self.id_fw[key].state for key in self.id_fw}
 
     @classmethod
     def from_dict(cls, m_dict):
