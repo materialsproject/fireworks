0248ee4f33a6da0fcb251b2c8fa07ee1372781e9
__sp__ <ongsp@eng.ucsd.edu>
2016-10-23 18:50:54 -0700

    Napoleon is now part of Sphinx 1.3

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ print(sys.path)
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'sphinxcontrib.napoleon']
+              'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.ifconfig', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
