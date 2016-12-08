6133748c4f78bff939cda3468149dda64954ff36
Donny Winston <dwinston@alum.mit.edu>
2016-12-08 11:13:18 -0800

    Accept app.secret_key from env var

--- fireworks/flask_site/app.py
+++ fireworks/flask_site/app.py
@@ -12,7 +12,9 @@ from functools import wraps
 
 app = Flask(__name__)
 app.use_reloader = True
-app.secret_key = 'super secret key'
+app.secret_key = os.environ.get(
+    "FWAPP_SECRET_KEY",
+    '0\x07)\x95\x96)\xb9\xdf1\xc0l4\x99\xc4\xf1\x88Jk\xb4lZ\xb2\x81X')
 
 hello = __name__
 lp = LaunchPad.from_dict(json.loads(os.environ["FWDB_CONFIG"]))
