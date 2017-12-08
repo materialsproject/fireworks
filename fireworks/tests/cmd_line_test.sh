9bebff37802e84388c50d7891fe8ab01835c16f9
Anubhav Jain <ajain@lbl.gov>
2017-12-08 11:28:41 -0800

    fix cmd_line_test to not overwrite a user database!!

--- fireworks/tests/cmd_line_test.sh
+++ fireworks/tests/cmd_line_test.sh
@@ -10,16 +10,16 @@ function test_cmd {
     return $status
 }
 
-test_cmd lpad reset --password `date "+%Y-%m-%d"`
-test_cmd lpad add fw_tutorials/firetask/fw_adder.yaml
+test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml reset --password `date "+%Y-%m-%d"`
+test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml add fw_tutorials/firetask/fw_adder.yaml
 for mode in "more" "less" "all"
 do
-    test_cmd lpad get_wflows -d $mode
-    test_cmd lpad get_fws -d $mode
+    test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml get_wflows -d $mode
+    test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml get_fws -d $mode
 done
-rlaunch singleshot
+test_cmd rlaunch -l fireworks/tests/my_launchpad_unittest.yaml singleshot
 
-test_cmd lpad append_wflow -i 1 -f fw_tutorials/firetask/fw_adder.yaml
-test_cmd lpad dump_wflow -i 1 -f test_dump_wflow.json && rm -f test_dump_wflow.json
-test_cmd rlaunch singleshot
-test_cmd lpad delete_wflows -i 1
+test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml append_wflow -i 1 -f fw_tutorials/firetask/fw_adder.yaml
+test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml dump_wflow -i 1 -f test_dump_wflow.json && rm -f test_dump_wflow.json
+test_cmd rlaunch -l fireworks/tests/my_launchpad_unittest.yaml singleshot
+test_cmd lpad -l fireworks/tests/my_launchpad_unittest.yaml delete_wflows -i 1
