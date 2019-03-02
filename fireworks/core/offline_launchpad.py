dbc2798488a4cc29d9b66dc54de67af63b13f89f
richardjgowers <richardjgowers@gmail.com>
2019-03-02 11:36:55 -0500

    added NIE stubs for everything

--- fireworks/core/offline_launchpad.py
+++ fireworks/core/offline_launchpad.py
@@ -39,6 +39,171 @@ class OfflineLaunchPad(object):
         self.fireworks = Collection('.launchpad', 'firework')
         self.workflows = Collection('.launchpad', 'workflow')
 
+    def to_dict(self):
+        raise NotImplementedError
+
+    def update_specs(self, fw_ids, spec_document, mongo=False):
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(cls, d):
+        raise NotImplementedError
+
+    @classmethod
+    def auto_loc(cls):
+        raise NotImplementedError
+
+    def reset(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def maintain(self, **kwargs):
+        raise NotImplementedError
+
+    def add_wf(self, wf, reassign_all=True):
+        """Add WF to LaunchPad"""
+        if isinstance(wf, Firework):
+            wf = Workflow.from_Firework(wf)
+
+        for fw_id in wf.root_fw_ids:
+            wf.id_fw[fw_id].state = 'READY'
+            wf.fw_states[fw_id] = 'READY'
+
+        # "insert" and get new mapping of ids
+        old_new = self._upsert_fws(list(wf.id_fw.values()),
+                                   reassign_all=reassign_all)
+        wf._reassign_ids(old_new)
+        self.workflows.write('1', wf.to_db_dict())
+
+        return old_new
+
+    def bulk_add_wfs(self, wfs):
+        raise NotImplementedError
+
+    def append_wf(self, new_wf, fw_ids, detour=False, pull_spec_mods=True):
+        raise NotImplementedError
+
+    def get_launch_by_id(self, launch_id):
+        raise NotImplementedError
+
+    def get_fw_dict_by_id(self, fw_id):
+        try:
+            fw_dict = self.fireworks.load(str(fw_id))
+        except FileNotFoundError:
+            raise ValueError
+
+        return fw_dict
+
+    def get_fw_by_id(self, fw_id):
+        return Firework.from_dict(self.get_fw_dict_by_id(fw_id))
+
+    def get_wf_by_fw_id(self, fw_id):
+        # search through all Workflows, looking for correct fw_id?
+        # or could have some index file, but this could get stale
+        raise NotImplementedError
+
+    def get_wf_by_fw_id_lzyfw(self, fw_id):
+        raise NotImplementedError
+
+    def delete_wf(self, fw_id, delete_launch_dirs=False):
+        raise NotImplementedError
+
+    def get_wf_summary_dict(self, fw_id, mode='more'):
+        raise NotImplementedError
+
+    def get_fw_ids(self, **kwargs):
+        raise NotImplementedError
+
+    def get_wf_ids(self, **kwargs):
+        raise NotImplementedError
+
+    def run_exists(self, fworker=None):
+        raise NotImplementedError
+
+    def future_run_exists(self, fworker=None):
+        raise NotImplementedError
+
+    def tuneup(self, bkground=True):
+        raise NotImplementedError
+
+    def pause_fw(self, fw_id):
+        raise NotImplementedError
+
+    def defuse_fw(self, fw_id, rerun_duplicates=True):
+        raise NotImplementedError
+
+    def reignite_fw(self, fw_id):
+        raise NotImplementedError
+
+    def resume_fw(self, fw_id):
+        raise NotImplementedError
+
+    def defuse_wf(self, fw_id, defuse_all_states=True):
+        raise NotImplementedError
+
+    def pause_wf(self, fw_id):
+        raise NotImplementedError
+
+    def reignite_wf(self, fw_id):
+        raise NotImplementedError
+
+    def archive_wf(self, fw_id):
+        raise NotImplementedError
+
+    def _restart_ids(self, next_fw_id, next_launch_id):
+        raise NotImplementedError
+
+    def _check_fw_for_uniqueness(self, m_fw):
+        raise NotImplementedError
+
+    def _get_a_fw_to_run(self, query=None, fw_id=None, checkout=True):
+        raise NotImplementedError
+
+    def _get_active_launch_ids(self):
+        raise NotImplementedError
+
+    def reserve_fws(slef, *args, **kwargs):
+        raise NotImplementedError
+
+    def get_fw_ids_from_reservation_id(self, reservation_id):
+        raise NotImplementedError
+
+    def cancel_reservation_by_reservation_id(self, reservation_id):
+        raise NotImplementedError
+
+    def get_reservation_id_from_fw_id(self, fw_id):
+        raise NotImplementedError
+
+    def cancel_reservation(self, launch_id):
+        raise NotImplementedError
+
+    def detect_unreserved(self, **kwargs):
+        raise NotImplementedError
+
+    def mark_fizzled(self, launch_id):
+        raise NotImplementedError
+
+    def detect_lostruns(self, **kwargs):
+        raise NotImplementedError
+
+    def set_reservation_id(self, launch_id, reservation_id):
+        raise NotImplementedError
+
+    def checkout_fw(self, fworker, launch_dir, fw_id=None, host=None,
+                    ip=None, state="RUNNING"):
+        raise NotImplementedError
+
+    def change_launch_dir(self, launch_id, launch_dir):
+        raise NotImplementedError
+
+    def restore_backup_data(self, launch_id, fw_id):
+        raise NotImplementedError
+
+    def complete_launch(self, launch_id, action=None, state='COMPLETED'):
+        raise NotImplementedError
+
+    def ping_launch(self, launch_id, ptime=None, checkpoint=None):
+        raise NotImplementedError
+
     def get_new_fw_id(self, quantity=1):
         try:
             next_id = self._metadata.read('next_fw_id')
@@ -47,6 +212,9 @@ class OfflineLaunchPad(object):
         self._metadata.write('next_fw_id', next_id + quantity)
 
         return next_id
+
+    def get_new_launch_id(self):
+        raise NotImplementedError
 
     def _upsert_fws(self, fws, reassign_all=False):
         """Insert into Fireworks 'collection'"""
@@ -73,19 +241,41 @@ class OfflineLaunchPad(object):
 
         return old_new
 
-    def add_wf(self, wf, reassign_all=True):
-        """Add WF to LaunchPad"""
-        if isinstance(wf, Firework):
-            wf = Workflow.from_Firework(wf)
+    def rerun_fw(self, fw_id, **kwargs):
+        raise NotImplementedError
 
-        for fw_id in wf.root_fw_ids:
-            wf.id_fw[fw_id].state = 'READY'
-            wf.fw_states[fw_id] = 'READY'
+    def get_recovery(self, fw_id, launch_id='last'):
+        raise NotImplementedError
 
-        # "insert" and get new mapping of ids
-        old_new = self._upsert_fws(list(wf.id_fw.values()),
-                                   reassign_all=reassign_all)
-        wf._reassign_ids(old_new)
-        self.workflows.write('1', wf.to_db_dict())
+    def _refresh_wf(self, fw_id):
+        raise NotImplementedError
 
-        return old_new
+    def _update_wf(self, wf, updated_ids):
+        raise NotImplementedError
+
+    def _steal_launches(self, thief_fw):
+        raise NotImplementedError
+
+    def set_priority(self, fw_id, priority):
+        raise NotImplementedError
+
+    def get_logdir(self):
+        raise NotImplementedError
+
+    def add_offline_run(self):
+        raise NotImplementedError
+
+    def recover_offline(self):
+        raise NotImplementedError
+
+    def forget_offline(self):
+        raise NotImplementedError
+
+    def get_tracker_data(self, fw_id):
+        raise NotImplementedError
+
+    def get_launchdir(self, fw-id, launch_idx=-1):
+        raise NotImplementedError
+
+    def log_message(self, level, message):
+        raise NotImplementedError
