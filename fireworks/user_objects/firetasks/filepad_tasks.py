ac2792ccbd6929f7771a4ab13459e4441758cc12
Johannes Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2019-07-17 22:37:20 +0200

    Undone text to binary filepad mod

--- fireworks/user_objects/firetasks/filepad_tasks.py
+++ fireworks/user_objects/firetasks/filepad_tasks.py
@@ -77,11 +77,8 @@ class GetFilesTask(FiretaskBase):
         for i, l in enumerate(self["identifiers"]):
             file_contents, doc = fpad.get_file(identifier=l)
             file_name = new_file_names[i] if new_file_names else doc["original_file_name"]
-            # Instead of worrying about encoding, just write binary data.
-            # Handling encoding correctly for text files is no longer
-            # a responsibility of Fireworks here.
-            with open(os.path.join(dest_dir, file_name), "wb") as f:
-                f.write(file_contents)
+            with open(os.path.join(dest_dir, file_name), "w") as f:
+                f.write(file_contents.decode())
 
 class GetFilesByQueryTask(FiretaskBase):
     """
