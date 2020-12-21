7b5a2270b939362db563649f61bb33392e951a9d
helgammal <hhelgammal@lbl.gov>
2020-12-21 12:25:06 -0800

    Fixing code style issue

--- fireworks/core/firework.py
+++ fireworks/core/firework.py
@@ -209,7 +209,7 @@ class Firework(FWSerializable):
     A Firework is a workflow step and might be contain several Firetasks.
     """
     STATE_RANKS = {'ARCHIVED': -2, 'FIZZLED': -1, 'DEFUSED': 0, 'PAUSED': 0,
-                   'WAITING': 1, 'READY': 2, 'RESERVED': 3, 'RUNNING': 4, 
+                   'WAITING': 1, 'READY': 2, 'RESERVED': 3, 'RUNNING': 4,
                    'CHECKPOINTED': 5, 'COMPLETED': 6}
 
     # note: if you modify this signature, you must also modify LazyFirework
