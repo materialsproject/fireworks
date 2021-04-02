23c1a321c03b1380abbe5291c35f54b3a61124a7
Adam Fekete <adam.fekete@kcl.ac.uk>
2021-04-02 03:41:01 +0200

    update urls in the templates

--- fireworks/flask_site/app.py
+++ fireworks/flask_site/app.py
@@ -17,12 +17,11 @@ import fireworks.flask_site.helpers as fwapp_util
 from fireworks.flask_site.util import jsonify
 
 app = Flask(__name__)
-app.use_reloader = True
+# app.use_reloader = True
 app.secret_key = os.environ.get(
     "FWAPP_SECRET_KEY",
     os.urandom(24))
 
-hello = __name__
 app.BASE_Q = {}
 app.BASE_Q_WF = {}
 
@@ -407,4 +406,19 @@ def simple(coll, interval, num_intervals):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    # https://dlukes.github.io/flask-wsgi-url-prefix.html
+
+    from fireworks import LaunchPad
+    from werkzeug.middleware.dispatcher import DispatcherMiddleware
+    # from werkzeug.wrappers import Response
+
+    app.wsgi_app = DispatcherMiddleware(
+        Response('Not Found', status=404),
+        {'/my-app': app.wsgi_app}
+    )
+
+    app.lp = LaunchPad()
+    # app.config["APPLICATION_ROOT"]='/test/'
+    # app.config["SCRIPT_NAME"]='/test/'
+    app.run(debug=True, port=8080, threaded=False)
+
