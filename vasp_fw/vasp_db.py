20f51756484db79cbebbaf9e71f25591a427bac9
Dexter Antonio <dexter.d.antonio@gmail.com>
2021-04-20 19:06:39 -0700

    changed host selection for hpc1

--- vasp_fw/vasp_db.py
+++ vasp_fw/vasp_db.py
@@ -109,20 +109,31 @@ class VASPDB(FiretaskBase):
 
     
     @staticmethod
-    def set_env_vars(host_name: str): 
-        if 'cori' in host_name or 'edison' in host_name or 'nid' in host_name:
-            #print(os.environ['HOSTNAME'])
-            os.environ['VASP_PP_PATH']='/global/homes/a/ark245/pseudopotentials/from_zhao_old'
-            os.environ['VASP_COMMAND']='NTASKS=`echo $SLURM_TASKS_PER_NODE|tr \'(\' \' \'|awk \'{print $1}\'`; NNODES=`scontrol show hostnames $SLURM_JOB_NODELIST|wc -l`; NCPU=`echo " $NTASKS * $NNODES " | bc`; echo "num_cpu=" $NCPU; srun -n $NCPU vasp_std | tee -a op.vasp'
+    def set_env_vars(): 
+        path_home = os.environ['HOME']
+        if path_home.startswith('/global/homes'):
+                host_name = 'cori'
+        elif path_home.startswith('/home/'):
+                host_name = 'hpc1'
+        elif path_home.startswith('/Users/'):
+                host_name = 'local'
+        elif path_home.startswith('/home1/'):
+                host_name = 'stampede'
+        
+        if 'hpc1' in host_name:
+            os.environ['VASP_PP_PATH']='/home/ark245/programs/vasp5.4.4/pseudopotentials/pseudo54/'
+            os.environ['VASP_COMMAND']='module load vasp/5.4.4pl2-vtst; NTASKS=`echo $SLURM_TASKS_PER_NODE|tr \'(\' \' \'|awk \'{print $1}\'`; NNODES=`scontrol show hostnames $SLURM_JOB_NODELIST|wc -l`; NCPU=`echo " $NTASKS * $NNODES " | bc`; echo "num_cpu=" $NCPU; srun -n $NCPU vasp_std| tee -a op.vasp'
+        elif 'cori' in host_name or 'edison' in host_name or 'nid' in host_name:
+            print(os.environ['HOSTNAME'])
+            os.environ['VASP_PP_PATH']='/global/homes/a/ark245/pseudopotentials/dont_use_from_zhao_old' #from_zhao_old'
+            os.environ['VASP_COMMAND']='NTASKS=`echo $SLURM_TASKS_PER_NODE|tr \'(\' \' \'|awk \'{print $1}\'`; NNODES=`scontrol show hostnames $SLURM_JOB_NODELIST|wc -l`; NCPU=`echo " $NTASKS * $NNODES " | bc`; echo "num_cpu=" $NCPU; srun -n $NCPU vasp_gam | tee -a op.vasp'
         elif 'stampede' in host_name:
-            #print(os.environ['HOSTNAME'])
             os.environ['VASP_PP_PATH']='/home1/05364/ark245/pseudopotentials/PBE54'
             os.environ['VASP_COMMAND']='module load vasp/5.4.4; export OMP_NUM_THREADS=1;rm op.vasp;mpirun -np $SLURM_NTASKS vasp_std_vtst | tee op.vasp'
         else:
-            raise ValueError('Invalid host_name. Please use either "cori" or "stampede"')
+            raise ValueError('Invalid host_name. Please use either "cori", "stampede" or "hpc1"')
 
     def run_task(self, fw_spec):
-        host_name = fw_spec['host_name']
         is_zeolite = fw_spec['is_zeolite']
         database_path = fw_spec['database_path']
         input_id = fw_spec['input_id']
@@ -133,8 +144,11 @@ class VASPDB(FiretaskBase):
         ivdw = fw_spec['ivdw']
         isif = fw_spec['isif']
         working_dir = os.getcwd()
+        output_dir = os.path.join(working_dir, 'vasp_output')
+        Path.mkdir(output_dir, exist_ok=True)
+        #os.environ['VASP_PP_PATH'] = output_dir
 
-        self.set_env_vars(host_name) 
+        self.set_env_vars() 
         db = connect(database_path)
         atoms = db.get_atoms(input_id)
         #atoms = read(input_filename, '-1')
