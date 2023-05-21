ba9a3dac7fb5669f12fedfc5daba00b8cf2d244e
Jimmy Shen <14003693+jmmshn@users.noreply.github.com>
2023-05-21 12:16:35 -0700

    [BUG FIX] remove `_id` from `archived_launches`
    I'm noticies some restarted jobs now rendering b/c of the `_id` fields are pull from the `archived_launches` which results in the following error in the webgui:
    
    ```
      File "/Users/shen9/local/Caskroom/miniconda/base/envs/mp/lib/python3.9/site-packages/flask/json/provider.py", line 122, in _default
        raise TypeError(f"Object of type {type(o).__name__} is not JSON serializable")
    ```

--- fireworks/flask_site/app.py
+++ fireworks/flask_site/app.py
@@ -162,6 +162,8 @@ def get_fw_details(fw_id):
     fw = app.lp.get_fw_dict_by_id(fw_id)
     for launch in fw["launches"]:
         del launch["_id"]
+    for launch in fw["archived_launches"]:
+        del launch["_id"]
     del fw["_id"]
     return jsonify(fw)
 
