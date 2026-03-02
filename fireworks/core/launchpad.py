238d4d8f5de41995b9572175b61453af9d33a30f
Ivan Kondov <ivan.kondov@kit.edu>
2026-03-02 18:12:43 +0100

    refactor launch recovery block, refactor get_recovery

--- fireworks/core/launchpad.py
+++ fireworks/core/launchpad.py
@@ -1722,13 +1722,17 @@ class LaunchPad(FWSerializable):
 
         # Launch recovery
         if recover_launch is not None:
-            recovery = self.get_recovery(fw_id, recover_launch)
-            recovery.update(_mode=recover_mode)
-            set_spec = recursive_dict({"$set": {"spec._recovery": recovery}})
-            if recover_mode == "prev_dir":
-                prev_dir = self.get_launch_by_id(recovery.get("_launch_id")).launch_dir
-                set_spec["$set"]["spec._launch_dir"] = prev_dir
-            self.fireworks.find_one_and_update({"fw_id": fw_id}, set_spec)
+            rec_launch_id = m_fw.launches[-1] if recover_launch == "last" else recover_launch
+            recovery = self.get_recovery(rec_launch_id)
+            if recovery:
+                recovery.update(_mode=recover_mode)
+                set_spec = recursive_dict({"$set": {"spec._recovery": recovery}})
+                if recover_mode == "prev_dir":
+                    launch_f = {'launch_id': recovery.get("_launch_id")}
+                    launch_p = {'launch_dir': True}
+                    prev_dir = self.launches.find_one(launch_f, launch_p)['launch_dir']
+                    set_spec["$set"]["spec._launch_dir"] = prev_dir
+                self.fireworks.find_one_and_update({"fw_id": fw_id}, set_spec)
 
         # If no launch recovery specified, unset the firework recovery spec
         else:
@@ -1756,17 +1760,19 @@ class LaunchPad(FWSerializable):
 
         return reruns
 
-    def get_recovery(self, fw_id, launch_id="last"):
-        """Function to get recovery data for a given fw and launch
+    def get_recovery(self, launch_id):
+        """Function to get recovery data for a given launch
         Args:
-            fw_id (int): fw id to get recovery data for
-            launch_id (int or 'last'): launch_id to get recovery data for, if 'last'
-                recovery data is generated from last launch.
+            launch_id (int): launch_id to get recovery data for
         """
-        m_fw = self.get_fw_by_id(fw_id)
-        launch = m_fw.launches[-1] if launch_id == "last" else self.get_launch_by_id(launch_id)
-        recovery = launch.state_history[-1].get("checkpoint")
-        recovery.update(_prev_dir=launch.launch_dir, _launch_id=launch.launch_id)
+        launch_f = {'launch_id': launch_id}
+        launch_p = {'launch_dir': True, 'state_history': True}
+        launch_dct = self.launches.find_one(launch_f, launch_p)
+        if not launch_dct['state_history']:
+            return None
+        recovery = launch_dct['state_history'][-1].get("checkpoint")
+        if recovery:
+            recovery.update(_prev_dir=launch_dct['launch_dir'], _launch_id=launch_id)
         return recovery
 
     def _refresh_wf(self, fw_id) -> None:
